f966779522024b39d81b1a8539a03d3ff8dbda95
Elden <eldeniyenden@gmail.com>
2019-07-14 01:33:24 +0900

    feat: make universalLogger usable in node.js

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
+const isNode = (typeof process !== 'undefined') && (process.release.name === 'node');
+
 export function logger(logTag: string) {
-  const tagSegment = `%c${logTag}%c `;
-  return function universalLoggerFn(format: string, ...args: any[]) {
-    const _format = tagSegment + format;
-    console.log(_format, 'color: #0aa8b3', 'color: black', ...args); // eslint-disable-line
-  };
+  return isNode
+    ? function nodeLoggerFn(format: string, ...args: any[]) {
+      const time = new Date().toISOString();
+      const firstArgument = `${time} \x1b[36m${logTag}\x1b[0m ${format}`;
+      console.log(firstArgument, ...args); // eslint-disable-line
+    }
+    : function universalLoggerFn(format: string, ...args: any[]) {
+      const _format = `%c${logTag}%c ${format}`;
+      console.log(_format, 'color: #0aa8b3', 'color: black', ...args); // eslint-disable-line
+    };
 }
